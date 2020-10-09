15d5802c4dc648857193712cf8e898496c76d752
Akshay Rai <akshaykumarrai.1rn16ec014@gmail.com>
2020-10-09 22:19:21 +0530

    updated main.py with new questions

--- main.py
+++ main.py
@@ -17,6 +17,51 @@ questions = [{
     "option4": "Taka",
     "correct_answer": "option1"
 
+},
+{
+    "question": "What is the Capital of Australia?",
+    "option1": "Sydney",
+    "option2": "Melbourne",
+    "option3": "Brisbane",
+    "option4": "Canberra",
+    "correct_answer": "option4"
+
+},
+{
+    "question": "What is the Capital of Uganda?",
+    "option1": "Lira",
+    "option2": "Mbarara",
+    "option3": "Kampala",
+    "option4": "Gulu",
+    "correct_answer": "option3"
+
+},
+{
+    "question": "What is the Capital of Myanmar?",
+    "option1": "Yangon",
+    "option2": "Magway",
+    "option3": "Monywa",
+    "option4": "Bago",
+    "correct_answer": "option1"
+
+},
+{
+    "question": "What is the Capital of USA?",
+    "option1": "Washington DC",
+    "option2": "New York",
+    "option3": "New Jersey",
+    "option4": "Las Vegas",
+    "correct_answer": "option4"
+
+},
+{
+    "question": "What is the Capital of England?",
+    "option1": "Birmingham",
+    "option2": "Edinburgh",
+    "option3": "Manchester",
+    "option4": "London",
+    "correct_answer": "option4"
+
 }]
 
 # RUN QUIZ 
