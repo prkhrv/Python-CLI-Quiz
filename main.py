1b8db773f513b42557f8fdac991da0799276766c
prkhrv <prakhar_varshney@outlook.com>
2020-10-10 19:51:22 +0530

    ADDED JSON FILE

--- main.py
+++ main.py
@@ -1,150 +1,15 @@
 from quiz import start_quiz
 import random
+import json
 
 # QUESTIONS 
-questions = [{
-    "question": "What is the Capital of Uttar Pradesh?",
-    "option1": "Kanpur",
-    "option2": "Lucknow",
-    "option3": "Noida",
-    "option4": "Varanasi",
-    "correct_answer": "option2"
-},
-{
-    "question": "What is the Currency of UAE?",
-    "option1": "Dirham",
-    "option2": "Rupee",
-    "option3": "Dollar",
-    "option4": "Taka",
-    "correct_answer": "option1"
-
-},
-{
-    "question": "What is the Capital of Australia?",
-    "option1": "Sydney",
-    "option2": "Melbourne",
-    "option3": "Brisbane",
-    "option4": "Canberra",
-    "correct_answer": "option4"
-
-},
-{
-    "question": "What is the Capital of Uganda?",
-    "option1": "Lira",
-    "option2": "Mbarara",
-    "option3": "Kampala",
-    "option4": "Gulu",
-    "correct_answer": "option3"
-
-},
-{
-    "question": "What is the Capital of Myanmar?",
-    "option1": "Yangon",
-    "option2": "Magway",
-    "option3": "Monywa",
-    "option4": "Bago",
-    "correct_answer": "option1"
-
-},
-{
-    "question": "What is the Capital of USA?",
-    "option1": "Washington DC",
-    "option2": "New York",
-    "option3": "New Jersey",
-    "option4": "Las Vegas",
-    "correct_answer": "option1"
-
-},
-{
-    "question": "What is the Capital of England?",
-    "option1": "Birmingham",
-    "option2": "Edinburgh",
-    "option3": "Manchester",
-    "option4": "London",
-    "correct_answer": "option4"
-},
-{
-    "question": "What does Argentina's name mean?",
-    "option1": "Majestic Skies",
-    "option2": "South Land",
-    "option3": "Land of silver",
-    "option4": "Mountainous",
-    "correct_answer": "option3"
-},
-{
-    "question": "Thousands of tourists come to Argentina every year to see what animals wintering there?",
-    "option1": "Southern right whales",
-    "option2": "Hummingbirds",
-    "option3": "Stingrays",
-    "option4": "All of the above",
-    "correct_answer": "option1"
-},
-{
-    "question": "Argentina is home to the highest peak in the Americas. What is it?",
-    "option1": "Cerro Bonete",
-    "option2": "Mount Aconcagua",
-    "option3": "Monte Pissis",
-    "option4": "Incahuasi",
-    "correct_answer": "option2"
-},
-{
-    "question": "When playing pato, Argentina's national sport, today's players use a leather ball with several handles. Originally, participants played with what?",
-    "option1": "A live duck in a basket",
-    "option2": "A roll of pampas",
-    "option3": "A coconut",
-    "option4": "A pig's bladder",
-    "correct_answer": "option1"
-},
-{
-    "question": "What are residents of Buenos Aires called?",
-    "option1": "Cariocas",
-    "option2": "Zamboangueño",
-    "option3": "Savoys",
-    "option4": "Porteño",
-    "correct_answer": "option4"
-},
-{
-    "question": "What is the name of the biggest technology company in South Korea?",
-    "option1": "Sony",
-    "option2": "Samsung",
-    "option3": "JVC",
-    "option4": "Hyundai Motors",
-    "correct_answer": "option2"
-},
-{
-    "question": "What is the world’s smallest bird?",
-    "option1": "Bee Hummingbird",
-    "option2": "Violetear",
-    "option3": "Goldcrest",
-    "option4": "Weebill",
-    "correct_answer": "option1"
-},
-{
-    "question": "What is the capital of Portugal?",
-    "option1": "Santa Cruz",
-    "option2": "Porto",
-    "option3": "Lagos",
-    "option4": "Lisbon",
-    "correct_answer": "option4"
-},
-{
-    "question": "What is a group of lions called?",
-    "option1": "A Murder",
-    "option2": "A Pride",
-    "option3": "A CatPack",
-    "option4": "A Herd",
-    "correct_answer": "option2"
-},
-{
-    "question": "What planet is known as the red planet?",
-    "option1": "Venus",
-    "option2": "Mercury",
-    "option3": "Mars",
-    "option4": "Jupiter",
-    "correct_answer": "option3"
-}]
+with open('./questions.json') as f:
+    data = json.load(f)
 
 
+questions = data["questions"]
+
+# RANDOM QUESTIONS
 random_questions = random.sample(questions, k=5)
 
 # RUN QUIZ 
