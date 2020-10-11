8f8e4d0f5249d44c91a527f207a3cdc2caec9601
prkhrv <prakhar_varshney@outlook.com>
2020-10-12 05:29:03 +0530

    small changes/ fixed SQL Question

--- quiz.py
+++ quiz.py
@@ -5,14 +5,14 @@ def start_quiz(questions):
     score = 0
     for i in range(0, len(questions)):
         print('--------------------------------------------------------')
-        print(f'Q{i+1}) {questions[i]["question"]}\n'); sleep(0.3)
+        print(f'Q{i+1}) {questions[i]["question"]}\n'); sleep(0.2)
         
         for j in range(1, 5):
-            print(f"({j}) {questions[i]['option'+str(j)]:30}", end='\t'); sleep(0.2)
+            print(f"({j}) {questions[i]['option'+str(j)]:30}", end='\t'); sleep(0.1)
             if not j%2: print()
 
         try:
-            answer = int(input("\nEnter your option number: ")); sleep(0.3)
+            answer = int(input("\nEnter your option number: ")); sleep(0.2)
             if not 0<answer<5:
                 print(chalk.yellow("Please enter a choice between 1 and 4\n")); sleep(0.4)
                 continue
