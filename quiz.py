fa88412141227d11ef4fab419bee5e709b003e4b
Abhiramborige <abhiramborige@gmail.com>
2020-10-21 11:32:19 +0530

    Code Flow controlled

--- quiz.py
+++ quiz.py
@@ -1,6 +1,14 @@
 from simple_chalk import chalk
 from time import sleep
 
+# Exception Handling here
+class InputError(Exception):
+    def __init__(self,value):
+        self.value=value
+def option_range(num):
+    if(num>4 or num<1):
+        raise InputError(chalk.yellow("Please enter a choice between 1 and 4"))
+
 def start_quiz(questions):
     score = 0
     for i in range(0, len(questions)):
@@ -11,14 +19,19 @@ def start_quiz(questions):
             print(f"({j}) {questions[i]['option'+str(j)]:30}", end='\t'); sleep(0.1)
             if not j%2: print()
 
-        try:
-            answer = int(input("\nEnter your option number: ")); sleep(0.2)
-            if not 0<answer<5:
-                print(chalk.yellow("Please enter a choice between 1 and 4\n")); sleep(0.4)
-                continue
-        except ValueError:
-            print(chalk.cyan('Invalid Choice\n')); sleep(0.4)
-            continue
+        # flag for controlling the loop
+        flag = False
+        while(flag==False):
+            try:
+                answer = int(input("\nEnter your option number: ")); sleep(0.2)
+                option_range(answer)
+                sleep(0.4)
+                flag = True
+            except InputError as error:
+                print(error)
+                sleep(0.4)
+            except ValueError:
+                print(chalk.cyan('Invalid Choice\n'))
             
         print()
         if(questions[i]["correct_answer"] == "option"+str(answer)):
